e33376d4638e8a48789da6f3766f669c22b531f4
lihuiyu-crosskey <1241803518@qq.com>
2019-06-28 16:37:23 +0800

    833

--- ow_auth_verify/config.py
+++ ow_auth_verify/config.py
@@ -37,6 +37,7 @@ elif check=='test':
     redis_refresh_token = {'host': '192.168.1.203', 'port': '6379', 'db': '2'}
     redis_user_info = {'host': '192.168.1.203', 'port': '6379', 'db': '3'}
     test={}
+    a={}
 
 
 class Config:
